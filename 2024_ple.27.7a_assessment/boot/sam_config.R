593d8dc809b69fcdb9ab3555d78bc757063193fd
Colin Millar <colinpmillar@gmail.com>
2024-06-26 17:55:01 +0200

    advice 2024

---
+++ 2024_ple.27.7a_assessment/boot/sam_config.R
@@ -0,0 +1,16 @@
+sourceTAF("../../../parameters.R")
+#sam_assessment <- "Ple.7a_2023_SW"
+
+sam_dir <-
+  paste0(
+    "https://stockassessment.org/datadisk/stockassessment/userdirs/user3/",
+    sam_assessment,
+    "/conf/"
+  )
+
+files <- "model.cfg"
+
+for (file in files) {
+  download(paste0(sam_dir, file))
+}
+
